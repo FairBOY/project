905df529888c87a7cc7d9ca8820acaf36c25c64c
FairBOY <kontodlakont@gmail.com>
2016-03-09 11:57:30 +0100

    Run migrations

--- db/migrate/20160308135957_create_orders.rb
+++ db/migrate/20160308135957_create_orders.rb
@@ -1,7 +1,23 @@
 class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
-
+    	# Order
+    	t.integer :user_id
+	    t.integer :service_id
+	    t.integer :repair_id
+	    t.text :description
+	    t.string :status, null: false, default: "nowe"
+	    t.integer :term, null: false, default: "7"
+	    t.datetime :date_of_adoption
+	    t.datetime :date_of_repair
+	    t.datetime :repair_end_date
+	    t.text :repair_attention
+	    t.float :price
+	    # Device
+	    t.string :type_of_device
+	    t.string :manufacturer
+	    t.string :model
+	    t.string :serial_number
       t.timestamps null: false
     end
   end
